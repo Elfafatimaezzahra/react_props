36b0d82825d53ee811ca4e2050eefcead103ca64
Elfafatimaezzahra <felfahimi@gmail.com>
2023-04-03 10:53:48 +0000

    modif

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Profile from './Profile/componentProfile';
+import myImg from './Profile/myImg.png'
 
 function App() {
   return (
     <div className="App">
-      <Profile/>
+      <Profile>
+        <img className='img' src={myImg} alt='my pic'/>
+      </Profile>
     </div>
   );
 }
--- src/Profile/componentProfile.jsx
+++ src/Profile/componentProfile.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import myImg from './myImg.png';
+
 
 
 const Profile = ({ fullName, bio, profession, children, handleName }) => {
   return (
     <div>
-      {children && <img className='img' src={children} alt="profile"  />}
-      {!children && <img className='img' src={myImg} alt="profile" />}
+      <div className='children'>{children}</div>
       <div className='fullname'>{fullName}</div>
       <div className='profession'>{profession}</div>
       <div className='bio'>{bio}</div>
@@ -25,7 +24,7 @@ Profile.propTypes = {
     fullName: PropTypes.string.isRequired,
     bio: PropTypes.string.isRequired,
     profession: PropTypes.string.isRequired,
-    children: PropTypes.string.isRequired,
+    children: PropTypes.element.isRequired,
     handleName: PropTypes.func.isRequired,
   };
 
@@ -35,7 +34,6 @@ Profile.defaultProps = {
   fullName: "Fatima-ezzahra El Fahimi",
   bio: " Hi, how are you? I m Fatima-ezzahra Elfahimi, I m 28 years old, I m JS developper.... ",  
   profession: "JS Developer",
-  children: "",
   handleName: (name) => alert(`Hey 👋, I called  ${name}`),
 };
 
